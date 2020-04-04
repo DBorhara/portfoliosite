383d7be809bdbb2ab9feee7f8f3812817433dfe1
Depak Borhara <depakborhara@gmail.com>
2020-04-04 00:32:02 -0400

    Added env variables

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+require('dotenv').config();
 const sgMail = require('@sendgrid/mail');
 const app = express();
 
